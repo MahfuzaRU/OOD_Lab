d7aa97f3cd8760a1da7ff885a245bd3863ef7e7d
mahfuza <s2112376111@ru.ac.bd>
2026-01-28 13:18:39 +0600

    add structure

--- builder_tutorial/BuilderPatternDemo.java
+++ builder_tutorial/BuilderPatternDemo.java
@@ -16,3 +16,35 @@ public class BuilderPatternDemo {
     }
 }
 
+
+/*
+Simulate a meal ordering system where different types of meals (e.g., vegetarian and non-vegetarian) are constructed using the Builder Design Pattern. A typical meal consists of a burger and a cold drink.
+
+The burger can be either a Veg Burger or a Chicken Burger, and it is packed using a wrapper.
+
+The cold drink can be either a Coke or a Pepsi, and it is packed in a bottle.
+
+Use the Builder Pattern to encapsulate the logic for constructing these meals step-by-step.
+
+Your program should:
+
+Display both vegetarian and non-vegetarian meals with the list of items included.
+
+Show the total cost of each meal.
+
+Draw a UML class diagram representing the system's structure.
+
+Item (interface)
+ ├── Burger (abstract)
+ │    ├── VegBurger
+ │    └── ChickenBurger
+ ├── ColdDrink (abstract)
+ │    ├── Coke
+ │    └── Pepsi
+
+Meal
+MealBuilder
+BuilderPatternDemo (main)
+
+
+*/
