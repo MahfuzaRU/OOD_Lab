544cdd84a486411fcb0f08b863ebc18f3b79828f
mahfuza <s2112376111@ru.ac.bd>
2026-02-02 15:05:16 +0600

    add chain of responsibility

---
+++ chainOfResponsibility/ErrorLogger.java
@@ -0,0 +1,11 @@
+//package chainOfResponsibility;
+
+public class ErrorLogger extends AbstractLogger {
+    public ErrorLogger(int level){
+        this.level = level;
+    }
+
+    protected void write(String message){
+        System.out.println("Error ConsolLogger :: Logger : " + message);
+    }
+}
