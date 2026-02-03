677debb32eb2b3ee1c10d46872369e78f8ae67e5
mahfuza <s2112376111@ru.ac.bd>
2026-02-03 19:34:01 +0600

    updated

--- Factory_Pattern/FactoryShapeDemo.java
+++ Factory_Pattern/FactoryShapeDemo.java
@@ -15,19 +15,3 @@ public class FactoryShapeDemo {
 }
 
 
-/*public class problem: 1
-
-Write a program to create various shapes like (Circle, Triangle, Rectangle) and calculate the area of 
-the shape. You should use Factory Pattern for shape creation so that all shapes can be created using 
-a single factory object.
-
-Each shape should have their defining properties (For example circle should have radius, triangle 
-should have length of its three side etc.) and a method named getArea() to calculate the area of the 
-shape.
-
-When creating a particular shape, you should initialize the shapes property with any default values 
-(ie. for circle, redius =1; for rectangle width =2 and height=1.5 etc.)
- {
-
-    
-} */
